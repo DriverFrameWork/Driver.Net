82e79bc31d80ee8382bcbe76e082fdc1915a89ba
zhangwenbin <zhangwenbin@outlook.com>
2016-07-04 12:04:34 +0800

    增加连接设置/日记

---
+++ Driver.Net.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Driver.Net.Infrastructure;
+using Driver.Net.Infrastructure.Logger;
+
+namespace Driver.Net.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory.GetLogger().Error("111asfasdfasdf");
+        }
+    }
+}
--- Driver.Net.Infrastructure/Logger/LoggerFactory.cs
+++ Driver.Net.Infrastructure/Logger/LoggerFactory.cs
@@ -4,17 +4,26 @@ using System.Linq;
 using System.Text;
 using log4net;
 using System.Threading.Tasks;
-[assembly: log4net.Config.XmlConfigurator(Watch = true,ConfigFile ="config/log4net.xml")]
+using System.IO;
+using Driver.Net.Infrastructure.Configuration;
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace Driver.Net.Infrastructure.Logger
 {
     public class LoggerFactory
     {
+        static LoggerFactory()
+        {
+            var fileName = AppSettingFinder.Query("logger:FileName");
+            var path = AppDomain.CurrentDomain.BaseDirectory + fileName; ;
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(path));
+
+        }
         private static ILog logger;
 
         public static ILog GetLogger()
         {
             if (logger == null)
-                logger = Logger.LoggerFactory.GetLogger();
+                logger =log4net.LogManager.GetLogger(typeof(LoggerFactory));
             return logger;
         }
     }
