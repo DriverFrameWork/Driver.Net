2dafcd923b850c0918460381adb70f700979fcb4
zhangwenbin <zhangwenbin@outlook.com>
2016-07-04 10:18:21 +0800

    增加连接设置/日记

--- Driver.Net.Infrastructure/Logger/LoggerFactory.cs
+++ Driver.Net.Infrastructure/Logger/LoggerFactory.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using log4net;
 using System.Threading.Tasks;
-
+[assembly: log4net.Config.XmlConfigurator(Watch = true,ConfigFile ="config/log4net.xml")]
 namespace Driver.Net.Infrastructure.Logger
 {
     public class LoggerFactory
     {
         private static ILog logger;
+
         public static ILog GetLogger()
         {
             if (logger == null)
